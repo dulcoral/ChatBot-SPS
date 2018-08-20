9e670543307ff81e94c731147b83f0e422e46691
Coral Rodriguez <dulcoral20@gmail.com>
2018-08-19 23:10:49 -0500

    funtion chat
    Add architecture

--- app/src/main/java/com/example/israel/myapplication/Adapters/ChatAdapter.java
+++ app/src/main/java/com/example/israel/myapplication/Adapters/ChatAdapter.java
@@ -14,24 +14,30 @@ import com.example.israel.myapplication.Model.QReply;
 import com.example.israel.myapplication.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import static com.example.israel.myapplication.MainActivity.getChatDialog;
-import static com.example.israel.myapplication.MainActivity.getHorizontalData;
-import static com.example.israel.myapplication.MainActivity.getQReplies;
 
-public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class ChatAdapter extends RecyclerView.Adapter {
 
     private Context context;
-    private ArrayList<Object> items;
+    private List<Object> items;
+    private final int MESSAGESEND = 0;
     private final int SIMPLEMESSAGE = 1;
     private final int CARROUSEL = 2;
-    private Boolean myMessage;
     private final int BTN = 3;
 
-    public ChatAdapter(Context context, ArrayList<Object> items, Boolean myMessage) {
+    public ChatAdapter(Context context, List<Object> items) {
         this.context = context;
-        this.items = items;
-        this.myMessage = myMessage;
+        if (items == null) {
+            this.items = new ArrayList<Object>();
+        } else {
+            this.items = items;
+        }
+    }
+
+    public void add(Object message) {
+        items.add(message);
+        notifyDataSetChanged();
     }
 
     @Override
@@ -41,62 +47,43 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         RecyclerView.ViewHolder holder;
         switch (viewType) {
             case SIMPLEMESSAGE:
-                if (myMessage) {
-                    view = inflater.inflate(R.layout.left_chat_bubble, parent, false);
-                } else {
-                    view = inflater.inflate(R.layout.right_chat_bubble, parent, false);
-                }
-                holder = new ChatBubbleViewHolder(view);
-                break;
-
+                view = inflater.inflate(R.layout.left_chat_bubble, parent, false);
+                return new ChatBubbleViewHolder(view);
+            case MESSAGESEND:
+                view = inflater.inflate(R.layout.right_chat_bubble, parent, false);
+                return new ChatBubbleViewHolder(view);
             case CARROUSEL:
                 view = inflater.inflate(R.layout.carrusel_layout, parent, false);
-                holder = new HorizontalViewHolder(view);
-                break;
+                return new CarrouselViewHolder(view);
             case BTN:
                 view = inflater.inflate(R.layout.quick_replies, parent, false);
-                holder = new QRepliesViewHolder(view);
-                break;
-            default:
-                if (myMessage) {
-                    view = inflater.inflate(R.layout.left_chat_bubble, parent, false);
-                } else {
-                    view = inflater.inflate(R.layout.right_chat_bubble, parent, false);
-                }
-                holder = new ChatBubbleViewHolder(view);
-                break;
+                return new QRepliesViewHolder(view);
         }
-
-
-        return holder;
+        return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder.getItemViewType() == CARROUSEL)
-            carrouselView((HorizontalViewHolder) holder);
-        else if (holder.getItemViewType() == BTN)
-            qRepliesView((QRepliesViewHolder) holder);
-        else if(holder.getItemViewType() == SIMPLEMESSAGE)
-            chatBView((ChatBubbleViewHolder) holder);
+        switch (holder.getItemViewType()) {
+            case SIMPLEMESSAGE:
+                ChatBubble message = (ChatBubble) items.get(position);
+                ((ChatBubbleViewHolder) holder).bind(message);
+                break;
+            case CARROUSEL:
+                ArrayList<CardResponse> cardResponses = (ArrayList<CardResponse>) items.get(position);
+                ((CarrouselViewHolder) holder).bind(cardResponses);
+                break;
+            case MESSAGESEND:
+                ChatBubble messageR = (ChatBubble) items.get(position);
+                ((ChatBubbleViewHolder) holder).bind(messageR);
+                break;
+            case BTN:
+                ArrayList<QReply> qReplies = (ArrayList<QReply>) items.get(position);
+                ((QRepliesViewHolder) holder).bind(qReplies);
+                break;
+        }
 
     }
-    private void chatBView(ChatBubbleViewHolder holder) {
-        ChatBubble chatB = getChatDialog();
-        holder.msg.setText(chatB.getContent());
-    }
-    private void qRepliesView(QRepliesViewHolder holder) {
-        QRepliesAdapter adapter = new QRepliesAdapter(getQReplies());
-        holder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
-        holder.recyclerView.setAdapter(adapter);
-    }
-
-    private void carrouselView(HorizontalViewHolder holder) {
-        CarrouselAdapter adapter = new CarrouselAdapter(getHorizontalData());
-        holder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-        holder.recyclerView.setAdapter(adapter);
-    }
-
 
     @Override
     public int getItemCount() {
@@ -105,23 +92,37 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemViewType(int position) {
-        if (items.get(position) instanceof ChatBubble)
-            return SIMPLEMESSAGE;
-        if (items.get(position) instanceof QReply)
-            return BTN;
-        if (items.get(position) instanceof CardResponse)
-            return CARROUSEL;
+        if (items.get(position) instanceof ChatBubble) {
+            ChatBubble chatBubble = (ChatBubble) items.get(position);
+            if (chatBubble.myMessage())
+                return SIMPLEMESSAGE;
+            else
+                return MESSAGESEND;
+        }
+        if (items.get(position) instanceof ArrayList) {
+            if (((ArrayList<?>) items.get(position)).get(0) instanceof CardResponse) {
+                return CARROUSEL;
+            } else
+                return BTN;
+        }
         return -1;
     }
 
-    public class HorizontalViewHolder extends RecyclerView.ViewHolder {
+    public class CarrouselViewHolder extends RecyclerView.ViewHolder {
 
         RecyclerView recyclerView;
 
-        HorizontalViewHolder(View itemView) {
+        CarrouselViewHolder(View itemView) {
             super(itemView);
             recyclerView = (RecyclerView) itemView.findViewById(R.id.inner_recyclerView);
         }
+
+        public void bind(ArrayList<CardResponse> cardResponses) {
+            CarrouselAdapter adapter = new CarrouselAdapter(cardResponses);
+            recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+            recyclerView.setAdapter(adapter);
+        }
+
     }
 
     public class ChatBubbleViewHolder extends RecyclerView.ViewHolder {
@@ -131,6 +132,11 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             super(itemView);
             msg = (TextView) itemView.findViewById(R.id.txt_msg);
         }
+
+        public void bind(ChatBubble message) {
+            msg.setText(message.getContent());
+
+        }
     }
 
     public class QRepliesViewHolder extends RecyclerView.ViewHolder {
@@ -139,6 +145,12 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         QRepliesViewHolder(View itemView) {
             super(itemView);
             recyclerView = (RecyclerView) itemView.findViewById(R.id.inner_recyclerView);
+        }
+
+        public void bind(ArrayList<QReply> qReplies) {
+            QRepliesAdapter adapter = new QRepliesAdapter(qReplies);
+            recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+            recyclerView.setAdapter(adapter);
         }
     }
 
